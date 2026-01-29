a505ed18886cba7c28979015c9f2e1d55051e49a
alessioongi <alessio.delmonte@edu-its.it>
2026-01-29 11:49:20 +0100

    inizializzazione progetto

---
+++ src/test/java/com/delmonte/serie_a/SerieAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delmonte.serie_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerieAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
